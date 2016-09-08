b9f09d76203af444d9acf7a7f891f341aaa54292
sulmanweb <sulmanweb@gmail.com>
2016-09-08 17:26:11 +0500

    setting the rack-cors

--- config/application.rb
+++ config/application.rb
@@ -27,10 +27,13 @@ module SimpleTodoApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
+    # Setting origins for the api hit
+    config.middleware.use Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete]
+        resource '*',
+                 :headers => :any,
+                 :methods => [:get, :post, :options, :delete, :put, :patch]
       end
     end
   end
